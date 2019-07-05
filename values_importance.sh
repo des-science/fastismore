0380ca9b91808ca77dde1c9d3b6df22888351a09
Otávio Alonso <alonso.otavio@gmail.com>
2019-07-04 21:08:07 -0300

    importance_sampling: first test with multinest

--- values_importance.sh
+++ values_importance.sh
@@ -5,10 +5,10 @@
 #
 # Script to generate values_importance.ini
 
-echo "%include values_wa.ini" > values_importance.ini
+echo "%include values.ini" > values_importance.ini
 echo "" >> values_importance.ini
 echo "[data_vector]" >> values_importance.ini
 for ((i=1; i<=$1; i++))
 do
-		echo "2pt_theory_$i = 0 0.5 1" >> values_importance.ini
+		echo "2pt_theory_$i = -10 0. 10" >> values_importance.ini
 done
