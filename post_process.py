a254748116dd2aecbb9e903c8a27d7714e63454b
Noah Weaverdyck <nweaverd@umich.edu>
2022-02-24 16:59:43 -0800

    minor bug fix from last commit

--- post_process.py
+++ post_process.py
@@ -463,7 +463,7 @@ class ImportanceChain(Chain):
         """If regularize=True (default), divide by maximum likelihood difference before computing weights. Cancels when normalize weights, and helps with overflow when large offset in loglikelihoods."""
         nonzero = (self.base.get_weights()!=0)
         likediff = self.data['new_like'] - self.data['old_like']
-        if regularize=True:
+        if regularize==True:
             maxdiff = np.max(likediff[nonzero])
         w = np.nan_to_num(np.exp(likediff - maxdiff))
         if 'extra_is_weight' in self.data.keys():
